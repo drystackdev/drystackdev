f7de761054df5317f0adb38b487173c922377fb0
Khan Trần <k.coder.282@gmail.com>
2026-07-16 00:01:11 +0700

    feat: update DrystackSetup component to make logo a link and improve error message display

--- packages/drystack/src/app/onboarding/setup.tsx
+++ packages/drystack/src/app/onboarding/setup.tsx
@@ -40,7 +40,7 @@ export function DrystackSetup(props: { config: GitHubConfig }) {
         }/settings/apps/new`}
         method="post"
       >
-        <Flex justifyContent="center">
+        <Flex as="a" href="/" justifyContent="center">
           <DrystackLogo />
         </Flex>
         <Text>Drystack doesn't have the required config.</Text>
